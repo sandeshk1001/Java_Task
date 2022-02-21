6a277c7361aef7585c7104dded1c13da1b30575f
Sandesh Kumawat <74561456+sandeshk1001@users.noreply.github.com>
2022-02-21 10:59:26 +0530

    Update Q2.java

--- Taskwork/javaassignment/Q2.java
+++ Taskwork/javaassignment/Q2.java
@@ -1,7 +1,22 @@
 package com.company.Taskwork.javaassignment;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Q2 {
     public static void main(String[] args) {
-        
+        System.out.println(checksum(Arrays.asList( 1, 10, 17, 20 , 50),31));
+
+    }
+    public static boolean checksum(List<Integer> l,Integer sum){
+        for(int i=0;i<l.size()-1;i++){
+            for (int j=1;j< l.size();j++){
+                if((l.get(i)) + (l.get(j)) == sum){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
+
